d385cee12c56b195e453cd16ddc096508f9a5393
alexander261 <kevin.pancho@uabc.edu.mx>
2025-03-21 18:31:13 +0000

    comandos

--- testing.asm
+++ testing.asm
@@ -1,3 +1,5 @@
+%include "./LIB/LIB/pc_io.inc"
+
 section	.text
 	global _start       ;must be declared for using gcc
 _start:                     ;tell linker entry point
@@ -6,6 +8,17 @@ _start:                     ;tell linker entry point
 	mov	ebx, 1	    ;file descriptor (stdout)
 	mov	eax, 4	    ;system call number (sys_write)
 	int	0x80        ;call kernel
+
+	mov al,"x"
+	call putchar
+
+	
+
+	call clrscr
+
+	mov edx,msg
+	call puts
+
 	mov	eax, 1	    ;system call number (sys_exit)
 	int	0x80        ;call kernel
 
